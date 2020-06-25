78618a9dd25b8575628d8e02f2671a8800a88106
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-24 21:17:19 -0700

    Update SQLQuery1.sql

--- SQL Server Lesson 1/SQL Server Lesson 1/SQLQuery1.sql
+++ SQL Server Lesson 1/SQL Server Lesson 1/SQLQuery1.sql
@@ -1,3 +1,6 @@
-insert into people (firstname, lastname, age) values('Trinh','Mai',20),('Phuc','Mai',13);
+create table unique_cats(
+id int not null, name varchar(100), 
+age int default 0, primary key(id));
 
-select * from people;
+insert into unique_cats(id, name, age) 
+values(1,'fred',23);
