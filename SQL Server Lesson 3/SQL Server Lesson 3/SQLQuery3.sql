7a77e068686c163b86c6e1f8be42a4cce9150962
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-27 17:17:52 -0700

    Create SQLQuery3.sql

---
+++ SQL Server Lesson 3/SQL Server Lesson 3/SQLQuery3.sql
@@ -0,0 +1,9 @@
+insert into shirts(article, color, size, last_worn)
+values ('t-shirt','white','S',10),
+       ('t-shirt','green','S',100),
+	   ('polo shirt','black','M',10),
+	   ('tank top','blue','S',50),
+	   ('t-shirt','pink','S',0),
+	   ('polo shirt','red','M',5),
+	   ('tank top','white','S',200),
+	   ('tank top','blue','M',15);
