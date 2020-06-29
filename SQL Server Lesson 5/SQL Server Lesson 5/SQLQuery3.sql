5c0f9d8be5445dd6101e2fde4b0fb9a7823baba0
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-28 19:48:28 -0700

    Update SQLQuery3.sql

--- SQL Server Lesson 5/SQL Server Lesson 5/SQLQuery3.sql
+++ SQL Server Lesson 5/SQL Server Lesson 5/SQLQuery3.sql
@@ -1,3 +1,4 @@
 select replace('hello world','hell','@@@');
 select replace('hello world','o','0');
 select replace('love hate death live', ' ',' and ');
+select substring(replace(title,'e','3'),1,10)from books;
