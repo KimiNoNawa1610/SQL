c4316d577c933e415ff03d8d0229d55b2832a7d1
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-29 20:12:14 -0700

    Create SQLQuery2.sql

---
+++ SQL Server Lesson 6/SQL Server Lesson 6/SQLQuery2.sql
@@ -0,0 +1,5 @@
+select author_lname from books;
+select distinct author_lname from books;
+select released_year from books;
+select distinct released_year from books;
+select distinct author_fname, author_lname from books;
