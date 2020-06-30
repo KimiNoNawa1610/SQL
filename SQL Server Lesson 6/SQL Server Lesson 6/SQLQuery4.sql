8d2c11e729dffbf39bedb948750b3431ae9be0d1
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-29 21:16:07 -0700

    Update SQLQuery4.sql

--- SQL Server Lesson 6/SQL Server Lesson 6/SQLQuery4.sql
+++ SQL Server Lesson 6/SQL Server Lesson 6/SQLQuery4.sql
@@ -3,3 +3,4 @@ select distinct title from books where pages like '6%';
 select concat(title,'-',released_year) as summary from books order by released_year desc;
 select distinct title, author_lname from books where author_lname like'%wal%';
 select top 3 title, released_year, stock_quantity from books order by stock_quantity ;
+select concat('my favorite author is ',author_fname, ' ',author_lname) as yell from books order by author_lname;
