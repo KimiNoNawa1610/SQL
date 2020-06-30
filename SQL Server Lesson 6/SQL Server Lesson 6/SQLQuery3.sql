299624060ecd9896bcf145ccd543b7bc587fde3a
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-29 21:14:26 -0700

    Update SQLQuery3.sql

--- SQL Server Lesson 6/SQL Server Lesson 6/SQLQuery3.sql
+++ SQL Server Lesson 6/SQL Server Lesson 6/SQLQuery3.sql
@@ -5,3 +5,4 @@ select title, author_fname, author_lname from books order by 2;
 select top 5 author_fname, author_lname from books order by author_lname desc;
 select author_lname from books where author_fname like '%da%';
 select author_lname from books where author_fname like '%da'
+select title from books where stock_quantity like'____';
