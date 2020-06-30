d86f8a6383b065752cc0b89bcfb466a009e37f2a
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-29 20:12:12 -0700

    Create SQLQuery1.sql

---
+++ SQL Server Lesson 6/SQL Server Lesson 6/SQLQuery1.sql
@@ -0,0 +1,4 @@
+insert into books(title,author_fname,author_lname,released_year,stock_quantity,pages)
+values ('10% happier','Dan','Harris',2014,18,256),
+	   ('fake_book','Freida','Harris',2001,287,428),
+	   ('Lincoln In The Bardo','George','Saunders',2017,1000,367);
