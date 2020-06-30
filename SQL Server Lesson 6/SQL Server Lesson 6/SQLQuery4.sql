47b55615351986c2e157aebbb56f397a66ff9c4f
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-29 21:14:28 -0700

    Create SQLQuery4.sql

---
+++ SQL Server Lesson 6/SQL Server Lesson 6/SQLQuery4.sql
@@ -0,0 +1,5 @@
+select distinct title from books where title like '%sto%';
+select distinct title from books where pages like '6%';
+select concat(title,'-',released_year) as summary from books order by released_year desc;
+select distinct title, author_lname from books where author_lname like'%wal%';
+select top 3 title, released_year, stock_quantity from books order by stock_quantity ;
