025fcbe89077280a64b85655b881cadd91bd1302
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-29 20:44:12 -0700

    Update SQLQuery3.sql

--- SQL Server Lesson 6/SQL Server Lesson 6/SQLQuery3.sql
+++ SQL Server Lesson 6/SQL Server Lesson 6/SQLQuery3.sql
@@ -4,3 +4,4 @@ select distinct released_year from books order by released_year;
 select title, author_fname, author_lname from books order by 2;
 select top 5 author_fname, author_lname from books order by author_lname desc;
 select author_lname from books where author_fname like '%da%';
+select author_lname from books where author_fname like '%da'
