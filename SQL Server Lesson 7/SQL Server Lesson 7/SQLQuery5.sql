98fc9d73acc7b642aa388fac5d046ce354a2645c
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-30 20:53:41 -0700

    update lesson 7

--- SQL Server Lesson 7/SQL Server Lesson 7/SQLQuery5.sql
+++ SQL Server Lesson 7/SQL Server Lesson 7/SQLQuery5.sql
@@ -1,1 +1,2 @@
 select author_fname, author_lname, min(released_year) from books group by author_fname, author_lname;
+select author_fname, author_lname, max(pages) from books group by author_fname,author_lname;
