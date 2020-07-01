09af7c7a44fc92f851193ec3c0a4c148cf5f6396
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-30 20:22:00 -0700

    Update SQLQuery2.sql

--- SQL Server Lesson 7/SQL Server Lesson 7/SQLQuery2.sql
+++ SQL Server Lesson 7/SQL Server Lesson 7/SQLQuery2.sql
@@ -1,2 +1,3 @@
 select title, count(title) as count from books group by title;
 select author_lname, count(author_lname) as count1 from books group by author_lname;
+select concat('there are ',count(released_year),' book(s) released in ', released_year) as count2 from books group by released_year;
