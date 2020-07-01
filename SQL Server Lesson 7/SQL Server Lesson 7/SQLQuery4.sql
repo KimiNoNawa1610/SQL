700f499ebec23486fde8b20920adde18edfd208b
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-30 20:35:31 -0700

    Update SQLQuery4.sql

--- SQL Server Lesson 7/SQL Server Lesson 7/SQLQuery4.sql
+++ SQL Server Lesson 7/SQL Server Lesson 7/SQLQuery4.sql
@@ -1,2 +1,4 @@
 select distinct title from books where pages=(select max(pages) from books);
+select top 1 title from books order by pages desc;
 select distinct title from books where pages=(select min(pages) from books);
+select top 1 title from books order by pages asc;
