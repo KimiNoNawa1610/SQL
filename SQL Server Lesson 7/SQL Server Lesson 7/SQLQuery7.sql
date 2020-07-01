aad8295364ab5ee0f9d70ec2a212e329c2622fbd
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-30 21:29:21 -0700

    update lesson 7

--- SQL Server Lesson 7/SQL Server Lesson 7/SQLQuery7.sql
+++ SQL Server Lesson 7/SQL Server Lesson 7/SQLQuery7.sql
@@ -1,3 +1,6 @@
 select count(*) from books;
 select released_year, count(distinct title) from books group by released_year;
 select sum(stock_quantity) from books; 
+select author_fname, author_lname, avg(released_year) from books group by author_fname, author_lname;
+select distinct author_fname, author_lname from books where pages=(select max(pages) from books);
+select released_year, count(distinct title), avg(pages) from books group by released_year;
