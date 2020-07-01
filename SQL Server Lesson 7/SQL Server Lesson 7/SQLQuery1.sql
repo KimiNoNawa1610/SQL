852ee94b3514d017f8161a4777e2e4a230a6801b
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-30 19:53:14 -0700

    Update SQLQuery1.sql

--- SQL Server Lesson 7/SQL Server Lesson 7/SQLQuery1.sql
+++ SQL Server Lesson 7/SQL Server Lesson 7/SQLQuery1.sql
@@ -1,3 +1,5 @@
 select * from books;
 select count(*) from books;
 select count(distinct author_fname) from books;
+select title from books where title like '%the%';
+select count(*) from books where title like '%the%';
