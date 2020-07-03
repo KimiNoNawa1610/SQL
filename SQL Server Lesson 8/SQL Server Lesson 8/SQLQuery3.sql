6877a8b909cd1a8d95900833d4e7978a06348672
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-02 19:52:43 -0700

    update lesson 8

--- SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery3.sql
+++ SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery3.sql
@@ -1,1 +1,4 @@
---decimal(5,2) 5 represent the total element of the number while 2 represent the total decimal point. For example: 777.77
+--decimal(5,2) 5 represent the total element of the number while 2 represent the total decimal point. For example: 777.77.
+--When we enter the number that outrange of the decimal, it will put the maximum number it can output.
+create table Items(name varchar (100),price decimal(5,2));
+insert into Items(name, price) values('Cookie',1.5),('Toilet Paper',10.99),('Rice',25.99);
