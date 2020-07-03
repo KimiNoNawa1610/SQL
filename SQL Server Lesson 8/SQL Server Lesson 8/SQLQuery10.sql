a9993fb8fad14b7957c311d2a8cc0b37d28f8bf3
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-02 21:00:24 -0700

    update lesson 8

---
+++ SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery10.sql
@@ -0,0 +1,4 @@
+select CURRENT_TIMESTAMP;
+--datetime values range from '1000-01-01 00:00:00' to '9999-12-31 23:59:59'
+--timestamp values range from '1970-01-01 00:00:01' to '2038-01-19 03:14:07'
+--timestamp takes less space than datetime
