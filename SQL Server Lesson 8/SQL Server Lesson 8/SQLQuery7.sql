455450c0baab332653b8e498a21a152d3cf0efd3
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-02 20:35:28 -0700

    update lesson 8

--- SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery7.sql
+++ SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery7.sql
@@ -3,3 +3,5 @@
 --now(): giver current date and time
 --getdate()=now().
 select getdate();
+insert into children(name,birthdate, birthtime, birthday) values('Jackson','2001-10-25','06:05:03',getdate());
+select * from children;
