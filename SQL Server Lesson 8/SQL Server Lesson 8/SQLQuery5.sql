b9ee7da8ad1293794f6f308049ca0001f1533a3c
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-02 20:30:44 -0700

    update lesson 8

--- SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery5.sql
+++ SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery5.sql
@@ -1,2 +1,5 @@
 --date is value of month, day, year without the time.
 --datetime is value of date with time
+--example:
+create table children(name varchar(20), birthdate date, birthtime time,birthday datetime);
+insert into children (name, birthdate,birthtime,birthday) values('David','2000-10-16','06:04:35','2000-10-16 06:04:35'),('John','2005-12-25','12:04:56','2005-12-25 12:04:56');
---
+++ SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery7.sql
@@ -0,0 +1,5 @@
+--curdate(): give the current date
+--curtime(): give the current time
+--now(): giver current date and time
+--getdate()=now().
+select getdate();
