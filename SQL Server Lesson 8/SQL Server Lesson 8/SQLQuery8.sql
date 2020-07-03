ff70583cff451daadc4a6382537e5a976221ed59
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-02 20:44:57 -0700

    update lesson 8

--- SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery8.sql
+++ SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery8.sql
@@ -1,2 +1,6 @@
 select name, day(birthdate) from children;
 select name, getdate() from children;
+select name, month(birthdate) from children;
+select name, year(birthdate) from children;
+select name, datename(dw,birthdate) from children;
+select name, datepart(dw,birthdate) from children;
