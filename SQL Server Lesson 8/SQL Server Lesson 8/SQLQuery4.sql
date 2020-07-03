bb6741bf83c640abf4eda7a6380f566d6adebfa9
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-02 20:01:26 -0700

    update lesson 8

--- SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery4.sql
+++ SQL Server Lesson 8/SQL Server Lesson 8/SQLQuery4.sql
@@ -1,1 +1,4 @@
 select * from Items;
+--float and double will store a larger number using less space but it comes at the cost of precision. 
+--Float will have precision issue after 7 digits. 
+--Double will have precision issue after 15 digits.
