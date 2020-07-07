53bd9da8636ecc7807a3c7d62892e750ab69fa27
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-06 20:05:45 -0700

    Update SQLQuery1.sql

--- SQL Server Lesson 9/SQL Server Lesson 9/SQLQuery1.sql
+++ SQL Server Lesson 9/SQL Server Lesson 9/SQLQuery1.sql
@@ -11,3 +11,7 @@ select title from books where author_fname='dave' or author_fname like'%w%';
 select title from books where released_year between 2000 and 2004;
 select title from books where released_year not between 2000 and 2010;
 select name from children where birthdate between '1980-01-01' and '2010-01-01';
+select title from books where author_lname in ('carver','lahiri','smith');
+select title from books where released_year not in(2000,2002,2004,2006,2008,2010,2012,2014,2016);
+select title from books where released_year %2 !=0;
+select title, released_year, case when released_year>=2000 then 'modern literature' else '20th century literature' end as Genre from books;
