74fcc7f34b40960d8ba47da508632b729a3ec4e8
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-06 20:32:52 -0700

    Update SQLQuery2.sql

--- SQL Server Lesson 9/SQL Server Lesson 9/SQLQuery2.sql
+++ SQL Server Lesson 9/SQL Server Lesson 9/SQLQuery2.sql
@@ -3,3 +3,4 @@ select title from books where author_lname in ('Eggers','Chabon');
 select title from books where author_lname='Lahiri' and released_year>2000;
 select title, pages from books where pages between 100 and 200;
 select title, author_lname from books where author_lname like'c%' or author_lname like 's%';
+select title, author_lname, case when title like '%stories%' then'Short Stories' when title like 'just kids and a heartbreaking work' then 'Memoir' else 'Novel' end as Type from books;
