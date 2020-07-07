9ae1cf2a35dbe409fc4c1867214d773d55bde1cc
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-06 19:39:04 -0700

    Update SQLQuery1.sql

--- SQL Server Lesson 9/SQL Server Lesson 9/SQLQuery1.sql
+++ SQL Server Lesson 9/SQL Server Lesson 9/SQLQuery1.sql
@@ -1,3 +1,6 @@
 select title from books where stock_quantity<300;
 select title from books where released_year!=2000;
 select title, author_lname from books where author_lname='Harris';
+select title from books where author_fname like'%t%';
+select title from books where author_fname not like '%w%';
+select title from books where released_year>2000;
