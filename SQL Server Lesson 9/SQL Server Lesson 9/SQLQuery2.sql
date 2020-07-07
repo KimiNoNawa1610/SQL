4cc1259e0fb807592020d03cfca105f6933b9119
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-06 20:20:56 -0700

    Update SQLQuery2.sql

--- SQL Server Lesson 9/SQL Server Lesson 9/SQLQuery2.sql
+++ SQL Server Lesson 9/SQL Server Lesson 9/SQLQuery2.sql
@@ -2,3 +2,4 @@ select title from books where released_year<1980;
 select title from books where author_lname in ('Eggers','Chabon');
 select title from books where author_lname='Lahiri' and released_year>2000;
 select title, pages from books where pages between 100 and 200;
+select title, author_lname from books where author_lname like'c%' or author_lname like 's%';
