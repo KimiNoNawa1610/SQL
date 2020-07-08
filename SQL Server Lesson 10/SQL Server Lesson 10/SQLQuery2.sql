045baf1cc784f39e09fc2491923fdb1793d24465
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-07 20:18:11 -0700

    Update SQLQuery2.sql

--- SQL Server Lesson 10/SQL Server Lesson 10/SQLQuery2.sql
+++ SQL Server Lesson 10/SQL Server Lesson 10/SQLQuery2.sql
@@ -3,3 +3,5 @@ Select* from orders;
 select * from customers where last_name='George';
 select * from orders where customer_id=1;
 select * from orders where customer_id=(select id from customers where last_name='George');
+/* Joining */
+Select * from customers,orders;
