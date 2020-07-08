2406354a428aa2e751dfda1a4cd298d6a8a674cd
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-07 20:30:25 -0700

    Update SQLQuery2.sql

--- SQL Server Lesson 10/SQL Server Lesson 10/SQLQuery2.sql
+++ SQL Server Lesson 10/SQL Server Lesson 10/SQLQuery2.sql
@@ -4,4 +4,4 @@ select * from customers where last_name='George';
 select * from orders where customer_id=1;
 select * from orders where customer_id=(select id from customers where last_name='George');
 /* Joining */
-Select * from customers,orders;
+Select * from customers,orders;-- implicit join
