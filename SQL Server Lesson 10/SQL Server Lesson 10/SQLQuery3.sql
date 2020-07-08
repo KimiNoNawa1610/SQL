e3447455fd81f8ba887115322e9d777ed1e6ac2f
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-07 20:43:31 -0700

    Update SQLQuery3.sql

--- SQL Server Lesson 10/SQL Server Lesson 10/SQLQuery3.sql
+++ SQL Server Lesson 10/SQL Server Lesson 10/SQLQuery3.sql
@@ -1,3 +1,6 @@
 /* Implicit Inner Join*/
 select* from customers, orders where customers.id=orders.customer_id;
 select first_name, last_name, order_date from customers, orders where customers.id=orders.customer_id;
+
+/*Explicit Inner Join*/
+select* from customers join orders on customers.id=orders.customer_id;
