3c6ef316e7128a387030ccb6ca766b91928a5fc4
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-08 20:27:48 -0700

    Update SQLQuery4.sql

--- SQL Server Lesson 10/SQL Server Lesson 10/SQLQuery4.sql
+++ SQL Server Lesson 10/SQL Server Lesson 10/SQLQuery4.sql
@@ -5,8 +5,10 @@ group by orders.customer_id,first_name,last_name order by total_spend desc;
 
 select * from customers left join orders on customers.id=orders.customer_id;
 
-select first_name, last_name, case when amount=null then 0 else sum(amount) end as total from customers left join orders on customers.id=orders.customer_id group by first_name, last_name;
+select first_name, last_name, coalesce(sum(amount),0) as total from customers left join orders on customers.id=orders.customer_id group by first_name, last_name;
 
 /*Right Join*/
 
 select * from customers right join orders on customers.id=orders.customer_id;
+
+select first_name, last_name, order_date, amount, sum(amount) from customers right join orders on customers.id=orders.customer_id group by first_name, last_name, order_date, amount;
