5af4244b3280acf3ad21bd4c26aaa1097aa8db3a
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-09 20:01:28 -0700

    Update SQLQuery3.sql

--- SQL Server Lesson 11/SQL Server Lesson 11/SQLQuery3.sql
+++ SQL Server Lesson 11/SQL Server Lesson 11/SQLQuery3.sql
@@ -1,3 +1,5 @@
 select title, rating from series join reviews on series.id=reviews.series_id;
 select title, avg(rating) as average_rating from series join reviews on series.id=reviews.series_id group by title order by average_rating;
 select first_name, last_name, rating from reviews inner join reviewers on reviewers.id=reviews.reviewer_id group by first_name, last_name,rating;
+select title from series left join reviews on series.id=reviews.series_id where rating is null;
+
