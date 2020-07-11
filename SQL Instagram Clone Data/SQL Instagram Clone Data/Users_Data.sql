8fdf0112717df2563a5147f9dfb28164e1158f4f
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-10 20:32:23 -0700

    Update Users_Data.sql

--- SQL Instagram Clone Data/SQL Instagram Clone Data/Users_Data.sql
+++ SQL Instagram Clone Data/SQL Instagram Clone Data/Users_Data.sql
@@ -1,11 +1,11 @@
 create database Istagram_Clone;
 
-create table users(
+create table Users(
 
 	id int primary key identity(1,1),
 
 	username varchar(100) unique not null,--Prevent users from getting same name
 
-	created_at date default getdate()
+	created_at datetime default getdate()
 	
 );
