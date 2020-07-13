2ccfb4805b740e0ee2b052f97881f8438cff16a3
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-12 23:58:19 -0700

    Update SQLQuery3.sql

--- SQL Server Lesson 11/SQL Server Lesson 11/SQLQuery3.sql
+++ SQL Server Lesson 11/SQL Server Lesson 11/SQLQuery3.sql
@@ -3,7 +3,9 @@ select title, rating from series join reviews on series.id=reviews.series_id;
 select title, avg(rating) as average_rating from series join reviews on series.id=reviews.series_id group by title order by average_rating;
 
 select first_name, last_name, rating from reviews inner join reviewers on reviewers.id=reviews.reviewer_id group by first_name, last_name,rating;
+
 select title from series left join reviews on series.id=reviews.series_id where rating is null;
+
 select genre, avg(rating) from series inner join reviews on series.id=reviews.series_id group by genre;
 select first_name, last_name, count(rating) as count, min(rating) as Min_rate, max(rating)as Max_rate, avg(rating) as Average_rating, 
 	case when avg(rating)>0 then'Active' else 'Inactive' end as Status 
