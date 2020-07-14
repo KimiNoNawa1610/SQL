bcb2f322b72f821752d6321403f81e0749780baf
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-13 21:38:20 -0700

    Update Solution.sql

--- Instagram_Clone_SQL_DATA/Solution.sql
+++ Instagram_Clone_SQL_DATA/Solution.sql
@@ -1,1 +1,3 @@
 select * from users order by created_at limit 5;/*select 5oldest users*/
+select username, dayname(created_at) as day, count(*) as total from users group by day order by total desc limit 2;
+select username from users left join photos on users.id=photos.user_id where photos.id is null; 
