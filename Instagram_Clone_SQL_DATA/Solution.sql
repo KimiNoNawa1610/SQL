24299f0f32e32dba5353be8c5c3e9c3a8f363aa1
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-14 19:46:12 -0700

    Update Solution.sql

--- Instagram_Clone_SQL_DATA/Solution.sql
+++ Instagram_Clone_SQL_DATA/Solution.sql
@@ -2,4 +2,5 @@ use Instaclone;
 select * from users order by created_at limit 5;/*select 5 oldest users*/
 select username, dayname(created_at) as day, count(*) as total from users group by day order by total desc limit 2;/*select 2 days in the week that have most active users*/
 select username from users left join photos on users.id=photos.user_id where photos.id is null; /*select non-aticve users*/
-select username,photos.image_url, count(*) as like_term from users left join photos on users.id=photos.user_id left join likes on photos.id=likes.photo_id group by photos.image_url order by like_term desc;
+select username,photos.image_url, count(*) as like_term from users left join photos on users.id=photos.user_id left join likes on photos.id=likes.photo_id group by photos.image_url order by like_term desc; /*select most liked photo*/
+select username, photos.image_url, avg() as average_post from users inner join photos on users.id=photos.user_id group by username order by average_post;
