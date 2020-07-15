8173150c2e222d6fdd55d6333d890142b1aa94c2
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-14 20:08:19 -0700

    Update Solution.sql

--- Instagram_Clone_SQL_DATA/Solution.sql
+++ Instagram_Clone_SQL_DATA/Solution.sql
@@ -5,4 +5,5 @@ select username from users left join photos on users.id=photos.user_id where pho
 select username,photos.image_url, count(*) as like_term from users left join photos on users.id=photos.user_id left join likes on photos.id=likes.photo_id group by photos.image_url order by like_term desc; /*select most liked photo*/
 select (select count(*) from photos)/
 	   (select count(*) from users) as average_post;/* Average user post */
-select tag_name, count(*) as usages from tags inner join photo_tags on tags.id=photo_tags.tag_id group by tag_name order by usages desc limit 10;
+select tag_name, count(*) as usages from tags inner join photo_tags on tags.id=photo_tags.tag_id group by tag_name order by usages desc limit 10;/* 10 most hashtags */
+select username, count(*) as total from users left join likes on likes.user_id=users.id  group by username having total=(select count(*) from photos);/* most active users */
