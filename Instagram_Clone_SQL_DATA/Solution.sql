1f1b6a80b88f82ab48c0cfa51ec797aedd131d0b
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-14 19:40:43 -0700

    Update Solution.sql

--- Instagram_Clone_SQL_DATA/Solution.sql
+++ Instagram_Clone_SQL_DATA/Solution.sql
@@ -2,4 +2,4 @@ select * from users order by created_at limit 5;/*select 5oldest users*/
 select username, dayname(created_at) as day, count(*) as total from users group by day order by total desc limit 2;
 select username from users left join photos on users.id=photos.user_id where photos.id is null; 
 use Instaclone;
-select username, count(*) as like_term from users left join photos on users.id=photos.user_id left join likes on photos.id=likes.photo_id group by username order by like_term desc;
+select username,photos.image_url, count(*) as like_term from users left join photos on users.id=photos.user_id left join likes on photos.id=likes.photo_id group by photos.image_url order by like_term desc;
