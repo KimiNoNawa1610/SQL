39f87382dff4fa7e94f836e79b47f37ad7743166
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-17 20:15:38 -0700

    Update Trigger.sql

--- SQL Server Lesson 12/Trigger.sql
+++ SQL Server Lesson 12/Trigger.sql
@@ -19,6 +19,37 @@ create trigger must_be_adult
 $$
 
 /*insert into users(username, age) values('Vâng',13);-- will cause error*/
+
 /*Example 2*/
 use Instaclone;
+DELIMITER $$
+create trigger cannot_follow_yourself
+	before insert on follows for each row
+    begin 
+		if new.follower_id=new.followee_id
+        then
+			signal sqlstate '65000'
+				set message_text='Cannot follow yourself';
+		end if;
+	end;
+$$
+/*insert into follows(follower_id,followee_id) values(5,5);-- will cause error;*/
 
+/*Example 3*/
+CREATE TABLE unfollows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+create trigger capture_unfollow
+	after delete on follows for each row
+    begin
+		set
+        follower_id=old.follower_id;
+        followee_id=old.followee_id;
+	end;
+$$
+    
