3a648927110eac17fdc3cd748f31cf47596aa0c8
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-17 19:55:12 -0700

    Create Trigger.sql

---
+++ SQL Server Lesson 12/Trigger.sql
@@ -0,0 +1,20 @@
+create database TriggerDemo;
+use TriggerDemo;
+create table users(
+	username varchar(100) unique,
+    age int
+);
+insert into users(username, age) values('tèo',13);
+
+DELIMITER $$
+create trigger must_be_adult
+	before insert on users for each row
+    begin
+		if new.age<18
+        then
+			signal sqlstate '45000'
+				set message_text="Must be an adult";
+                end if;
+                end;
+                $$
+
